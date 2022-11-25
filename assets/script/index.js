23ccf3ea18177189b4640009134f5e13712de778
Apricosma <113317992+Apricosma@users.noreply.github.com>
2022-11-25 13:52:39 -0600

    Fix validation and add more style

--- assets/script/index.js
+++ assets/script/index.js
@@ -23,12 +23,11 @@ class Contact {
     }
 
     set personName(name) {
-        if (name instanceof Contact && name.length > 0) {
-            this.#name = name;
+        if (1 === 1) {
+            this.#name = name;        
         } else {
-            console.log(`bad`);
+            throw ''
         }
-        
     }
 
     get personName() {
@@ -53,9 +52,13 @@ class Contact {
 
     render() {
         let element = document.createElement('div');
-
-        contactContainer.appendChild(element);
+        contactContainer.prepend(element);
         element.classList.add('contact');
+
+        // cross
+        let cross = document.createElement('a');
+        element.appendChild(cross);
+        cross.classList.add('close');
 
         // name
         let namePara = document.createElement('p');
@@ -75,9 +78,17 @@ class Contact {
         // delete element
         element.addEventListener('click', function () {
             if (element.matches('div')) {
-                element.remove();
+                let seconds = 0.5;
+                element.style.transition = 'opacity '+seconds+'s ease';
+
+                element.style.opacity = 0;
+                setTimeout(function() {
+                    element.remove();
+                }, 500);
+                count--;
+                head.innerHTML = `Contacts (${count})`
             }
-        })
+        })      
     }
 
     listContacts() {
@@ -93,20 +104,63 @@ const contactInput = select('.contact-input');
 const submit = select('.submit');
 const contactContainer = select('.contact-container');
 const contactElement = select('.contact');
+const errors = select('.errors');
+const head = select('.head');
 
+let count = 0;
 
 onEvent('click', submit, function () {
-    let contactString = contactInput.value;
-    let values = contactString.split(', ');
+    const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
+    const nameRegex = /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/
 
-    let name = values[0];
+        let valid = false;
+        if (contactInput.value === '' || contactInput.value == null){
+            errors.innerHTML = 'You must have an input';
+            throw new TypeError('You must have an input')
+        }
+        valid = true;
 
+        if (valid) {
+            let contactString = contactInput.value;
+            let values = contactString.split(', ');    
+            if (values.length < 3){
+                errors.innerHTML = 'Please make sure you have Name, City, Email';
+                throw new TypeError('Please make sure you have Name, City, Email');
+            }       
 
-    let city = values[1];
-    let email = values[2];
+            // gets all the values from array
+            let name = values[0];
+            let city = values[1];
+            let email = values[2];
 
+            if (!nameRegex.test(name)) {
+                errors.innerHTML = 'Please use first and last name';
+                throw new TypeError('Invalid name')
+            }
 
-    let person = new Contact(name, city, email);
-    console.log(person);
-    person.listContacts();
+            if (city <= 1) {
+                errors.innerHTML = 'City name must be longer than one';
+                throw new TypeError('City must be greater than 1')
+            }
+
+            if (!emailRegex.test(email)) {
+                errors.innerHTML = 'Invalid email';
+                throw new TypeError('Invalid email');
+            }
+
+            count++;
+            head.innerHTML = `Contacts (${count})` 
+            let person = new Contact(name, city, email);
+            errors.innerHTML = ''; // clears errors
+            
+            person.listContacts();
+        }
+})
+
+// enter key submission
+contactInput.addEventListener('keypress', function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        submit.click();
+    }
 })
